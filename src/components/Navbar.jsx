85662de62997aeb2775bf7547f3e2be9b9e3f487
kennedybabu <kennedynjogu8@gmail.com>
2022-06-08 13:52:58 +0300

    feature: authenticate and limit page access

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import  { UserAuth} from '../context/AuthContext'
 
 const Navbar = () => {
   const {user, logOut} = UserAuth()
-  console.log(user)
+
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logOut()
+      navigate('/')
+    } catch (error){
+      console.log(error)
+    }
+  }
 
   return (
     <div className='w-full flex justify-between items-center p-4 absolute top-0 z-[100]'>
       <Link to='/'>
           <h1 className='text-3xl text-red-600 uppercase font-bold cursor-pointer'>Netflix</h1>
       </Link>
+       {user?.email ? (
+          <div>
+            <Link to='/account'>
+                <button className='py-2 px-4 text-white cursor-pointer'>Account</button>
+            </Link>
+            <button onClick={handleLogout} className='py-2 px-4 text-white ml-4 cursor-pointer bg-red-600 rounded'>Log out</button>
+        </div>
+       ) : (
         <div>
           <Link to='/login'>
-               <button className='py-2 px-4 text-white cursor-pointer'>Sign In</button>
+              <button className='py-2 px-4 text-white cursor-pointer'>Sign In</button>
           </Link>
           <Link to='/signup'>
-                <button className='py-2 px-4 text-white ml-4 cursor-pointer bg-red-600 rounded'>Sign Up</button>
+                <button  className='py-2 px-4 text-white ml-4 cursor-pointer bg-red-600 rounded'>Sign Up</button>
           </Link>
-        </div>
+      </div>
+       )}
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const {user, logIn} =UserAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError('')
+        try {
+            await logIn(email, password)
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+            setError(error.message)
+        }
+    }
   return (
     <div>
         <div className='w-full h-screen'>
@@ -11,9 +29,10 @@ const Login = () => {
                 <div className='max-w-[450px]  h-[600px] mx-auto bg-black/75 text-white'>
                     <div className='max-w-[320px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold'>Sign In</h1>
-                        <form className='w-full flex flex-col py-4'>
-                            <input className='p-3 my-2 bg-gray-700 rounded' type="email"  placeholder='Email' autoComplete='email'/>
-                            <input className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
+                        {error ? <p className='p-3 bg-red-500 my-2'>{error}</p> : null}
+                        <form className='w-full flex flex-col py-4' onSubmit={handleSubmit}>
+                            <input onChange={(e) => setEmail(e.target.value)} className='p-3 my-2 bg-gray-700 rounded' type="email"  placeholder='Email' autoComplete='email'/>
+                            <input onChange={(e) => setPassword(e.target.value)} className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
                             <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
                             <div className='flex justify-between items-center text-sm text-gray-600'>
                                 <p><input type="checkbox" className='mr-2'/>Remember Me</p>
